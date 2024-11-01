9aad5b27c8175e1438e425b5a2d16f720e93cea0
💖 Chloe 💖 <gaycoderprincess@proton.me>
2024-11-01 19:45:31 +0100

    rename palette, it's not a pointer

--- types/HUD.h
+++ types/HUD.h
@@ -4,7 +4,7 @@ public:
 	bool bVisible;
 };
 
-auto pPalette = (uint32_t*)0x9298B70;
+auto gPalette = (uint32_t*)0x9298B70;
 
 auto AddHUDKeyword = (void(*)(const char*, int(__cdecl*)(wchar_t*, size_t, void*, void*), void*))0x4ECB20;
 auto LoadHUDFromDB = (void*(__thiscall*)(void*, const char*, void*))0x4EB880;
